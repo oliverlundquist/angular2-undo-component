32d11a369d5fd07a6334fa7e5108fa6d5f8dd4e2
oliverlundquist <oliver@mystore.no>
2016-07-20 11:43:29 +0200

    cleaned-up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,71 +12,50 @@ import * as Immutable from 'immutable';
 export class AppComponent {
     title = 'app works!';
 
-    private reducer1: Redux.Reducer<Object>;
-    private reducer2: Redux.Reducer<Object>;
-    private reducers: Redux.ReducersMapObject;
-    private compound: Redux.Store<Object>;
+    private reducer: Redux.Reducer<Object>;
+    private state: Redux.Store<Object>;
     private history: Immutable.List<Object>;
-    // private loggerz: Redux.Middleware;
-    private loggerz: any;
-    private lister: Immutable.List<Object>;
+    private manager: Redux.Middleware;
 
     constructor() {
+        this.reducer = this.stateReducer.bind(this);
         this.history = Immutable.List.of({});
-        this.loggerz = store => next => action => {
-            // let newState = Immutable.fromJS(store.getState()).toObject();
-            this.history = this.history.push(store.getState().reducer1.toArray()
-                                                .concat(store.getState().reducer2.toArray()));
-            // console.log(this.history.toArray());
-            return next(action);
+        this.manager = this.stateManager.bind(this);
+        this.state   = Redux.createStore(this.reducer, Redux.applyMiddleware(this.manager));
+
+        // setup subscriber
+        this.state.subscribe(this.stateHandler.bind(this));
+
+        // fire event
+        this.state.dispatch({ type: 'SOME_ACTION' });
+        // this.state.dispatch({ type: 'SOME_ACTION' });
+    }
+
+    stateReducer(state:Immutable.Map<String,Number>, action:Object) {
+        let timestamp = +(new Date()).getTime();
+        if (typeof state === 'undefined') { return Immutable.Map({time: timestamp}) }
+        console.log(state.toJS());
+        return state.set('time', timestamp);
+    }
+
+    stateHandler() {
+        //
+    }
+
+    stateManager(store):Redux.Dispatch<Object> {
+        return next => action => {
+            // Before Middleware
+            // ...
+
+            // Fire Reducers & Subscribers
+            let result = next(action);
+
+            // After Middleware
+            this.history = this.history.push(store.getState().toJS());
+
+            // End of Lifecycle
+            return result;
         }
-
-        this.reducer1 = (s = Immutable.Map({ count1: 0 }), a) => s.set('count1', (s.get('count1') + 1))
-        this.reducer2 = (s = Immutable.Map({ count2: 0 }), a) => s.set('count2', (s.get('count2') + 2))
-        this.reducers = { 'reducer1' : this.reducer1, 'reducer2': this.reducer2 };
-        this.compound = Redux.createStore(
-                Redux.combineReducers(this.reducers),
-                Redux.applyMiddleware(this.loggerz)
-        );
-
-
-        this.registerCallback();
-
-        this.dispatchAction();
-        // this.dispatchAction();
-        // this.dispatchAction();
-
-    }
-
-    dispatchAction () {
-        this.compound.dispatch({ type: 'SOME_ACTION' });
-        this.compound.dispatch({ type: 'SOME_ACTION' });
-        this.compound.dispatch({ type: 'SOME_ACTION' });
-    }
-
-    registerCallback() {
-        this.compound.subscribe(() => {
-            // console.log('subscribing', this.compound.getState());
-            console.log(this.history.toArray());
-        })
     }
 
 }
-
-// type Mixtype = String|Number;
-
-// class SomeClass<T extends Mixtype> {
-//     somemethod(arg: T): T {
-//         return arg;
-//     }
-// }
-
-// var somevar = new SomeClass<Number>();
-// console.log(somevar.somemethod(123));
-// console.log(somevar.somemethod('hey')); // error
-// console.log(somevar.somemethod(true)); // error
-
-
-
-
-
